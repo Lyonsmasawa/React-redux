8dbd2cd20ab9d76e22b3c0c11f941e5e985fec8b
Lyonsmasawa <lyonsmasawa@gmail.com>
2023-04-16 22:04:05 -0700

    react redux

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CakeContainer from './Components/CakeContainer'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </Provider>
   )
 }
+
 
 export default App
